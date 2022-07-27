07bada02b8b037e615a020df3f00026391534a08
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-07-27 15:16:56 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
