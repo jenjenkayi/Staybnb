6df9d4add3248291a0c231365d6fc4b33d3b46d6
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-06 22:34:32 -0700

    delete a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -120,7 +120,7 @@ router.delete('/:bookingId', requireAuth, async (req, res) => {
             })
     }
 
-    if (req.params.bookingId === req.user.id) {
+    if (booking.userId !== req.user.id) {
         res.status(403)
         return res.json({
             "message": "Forbidden",
