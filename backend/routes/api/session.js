af6faf8de62b2eab8a3ccb4560373a830a72497e
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-07 22:28:55 -0700

    get current user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -57,9 +57,7 @@ router.delete('/', (_req, res) => {
 router.get('/', restoreUser, (req, res) => {
         const { user } = req;
         if (user) {
-            return res.json({
-                user: user.toSafeObject()
-            });
+            return res.json(user.toSafeObject());
         } else return res.json({});
     }
 );
