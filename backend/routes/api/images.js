d1731e86d08a1c2cd85eb542024d1bb9d3d83936
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-08 07:56:34 -0700

    edit a booking

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -22,7 +22,7 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
         })
     }
 
-    if (image.userId === req.user.id) {
+    if (image.userId !== req.user.id) {
         res.status(403)
         return res.json({
             "message": "Forbidden",
