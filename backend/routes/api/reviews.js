92853cc7c139f9be6bf2ca04432351df5f051e5a
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-08 00:16:03 -0700

    add image to a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -113,7 +113,7 @@ router.put('/:reviewId', requireAuth, validateReview, async (req, res) => {
         )
     }
 
-    if (Review.id !== req.user.id) {
+    if (Review.userId !== req.user.id) {
         res.status(403)
         return res.json({
             "message": "Forbidden",
