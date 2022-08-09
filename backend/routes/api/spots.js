db079956e050027f388437e55cb0dff30ed0231d
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-09 08:08:12 -0700

    get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -156,7 +156,7 @@ router.get('/', async (req, res) => {
 
             if (previewImage) {
                 spot.dataValues.previewImage = previewImage.dataValues.url
-                spot.dataValues.avgRating = avgRating
+                spot.dataValues.avgRating = parseFloat(Number(avgRating)).toFixed(1);
             }
         }
 
