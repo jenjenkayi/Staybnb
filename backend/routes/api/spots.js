d5a0246423003d739d576ce01ad5931e926ca1d4
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-11 08:58:55 -0700

    edit a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -347,13 +347,13 @@ router.put('/:spotId', requireAuth, validateSpot, async (req, res) => {
             )
         }
     
-    if (spot.ownerId !== req.user.id) {
-        res.status(403)
-        return res.json({
-            "message": "Forbidden",
-            "statusCode": 403
-        });
-    }
+    // if (spot.ownerId !== req.user.id) {
+    //     res.status(403)
+    //     return res.json({
+    //         "message": "Forbidden",
+    //         "statusCode": 403
+    //     });
+    // }
 
     const { address, city, state, country, lat, lng, name, description, price } = req.body
     
