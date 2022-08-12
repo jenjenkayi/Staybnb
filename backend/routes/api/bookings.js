a49fd4d34d91823c5417e0a52738f125e92365e5
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-12 13:55:06 -0700

    delete a review

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const { requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
