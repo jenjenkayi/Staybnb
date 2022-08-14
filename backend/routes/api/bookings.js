800ced98a6ecb26ec9fe0fa0815b9973441ef02b
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-14 12:27:34 -0700

    edit a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -67,13 +67,13 @@ router.put('/:bookingId', requireAuth, async (req, res) => {
         })
     }
     
-    if (booking.userId !== req.user.id) {
-        res.status(403)
-        return res.json({
-            "message": "Forbidden",
-            "statusCode": 403
-        });
-    }
+    // if (booking.userId !== req.user.id) {
+    //     res.status(403)
+    //     return res.json({
+    //         "message": "Forbidden",
+    //         "statusCode": 403
+    //     });
+    // }
 
     const { startDate, endDate } = req.body;
     booking.startDate = startDate,
