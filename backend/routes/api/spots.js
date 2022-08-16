6cb9428bf3da09af8b7d00aebbe70088ef99f5ae
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-16 14:47:27 -0700

    create a booking for a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -573,17 +573,17 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
         }
     }
 
-    if (bookings.length >= 1) {
-        res.status(403)
-        res.json({
-            "message": "Sorry, this spot is already booked for the specified dates",
-            "statusCode": 403,
-            "errors": {
-                "startDate": "Start date conflicts with an existing booking",
-                "endDate": "End date conflicts with an existing booking"
-            }
-        })
-    }
+    // if (bookings.length >= 1) {
+    //     res.status(403)
+    //     res.json({
+    //         "message": "Sorry, this spot is already booked for the specified dates",
+    //         "statusCode": 403,
+    //         "errors": {
+    //             "startDate": "Start date conflicts with an existing booking",
+    //             "endDate": "End date conflicts with an existing booking"
+    //         }
+    //     })
+    // }
 
     const newBooking = await Booking.create({
             spotId: req.params.spotId,
