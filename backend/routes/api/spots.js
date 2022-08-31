7ed3849cf1724a3d71b90950dcd315008753ef85
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-31 13:51:53 -0700

    updated avgRating to 1 decimal

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -153,8 +153,8 @@ router.get('/', validatePagination, async (req, res) => {
 
             if (previewImage) {
                 spot.dataValues.previewImage = previewImage.url
+                spot.dataValues.avgRating = parseFloat(avgRating).toFixed(1);
             }
-                spot.dataValues.avgRating = parseFloat(spot.dataValues.avgRating).toFixed(1);
                 spot.dataValues.lat = parseFloat(spot.dataValues.lat);
                 spot.dataValues.lng = parseFloat(spot.dataValues.lng);
         }
@@ -202,7 +202,8 @@ router.get('/current', requireAuth, async (req, res) => {
         }
         spot.dataValues.lat = parseFloat(spot.dataValues.lat);
         spot.dataValues.lng = parseFloat(spot.dataValues.lng);
-        spot.dataValues.avgRating = parseFloat(spot.dataValues.avgRating).toFixed(1);
+        spot.dataValues.avgRating = Number.parseFloat(spot.dataValues.avgRating).toFixed(1);
+        // spot.dataValues.avgRating = parseFloat(Number(spot.dataValues.avgRating)).toFixed(1);;
     }
     return res.json({Spots: currentUserSpots})
 })
