773fd561522765df384833049c0938c1beb935ae
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-08-31 08:55:07 -0700

    get bookings of current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -26,7 +26,7 @@ router.get('/current', requireAuth, async (req, res) => {
     
         const currentUserSpots = await Spot.findAll({
             where: { id: booking.dataValues.spotId },
-            attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+            // attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
         })
 
         for (let i = 0; i < currentUserSpots.length; i++) {
