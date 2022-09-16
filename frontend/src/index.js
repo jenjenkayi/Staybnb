1ed8cdd1c32f81d7e6c0fc0fff66bd763142fb50
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-09-15 19:54:25 -0700

    phase 1 done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -10,7 +10,7 @@ import { restoreCSRF, csrfFetch } from './store/csrf';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
+  // restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
@@ -18,11 +18,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
-    <ReduxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ReduxProvider>
+    </Provider>
   );
 }
 
