2dddaa9eb0b336a67e83d5f3043816eafb4d26f5
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-09-17 12:51:17 -0700

    added components createSpot

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-const CreateSpotForm = ({ hideForm }) => {
+const CreateSpotForm = ({ }) => {
     const history = useHistory();
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
